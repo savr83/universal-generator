35d0c2f98b052ebadc851b26ca642dfe9e651e2e
Pavel Sokolov <comradepashka@gmail.com>
2018-09-17 22:17:55 +0300

    Mailkit: real rule percentage changed to avg multiplier

--- app/Mailkit/Rule.php
+++ app/Mailkit/Rule.php
@@ -13,9 +13,10 @@ class Rule extends Model
     public function getPriorityAttribute()
     {
         $plannedPercentage = $this->weight / self::$currentRuleSet->sum('weight');
-        $realPercentage = (self::$currentRuleSet->sum('counter') == 0) ? 0 : ($this->counter / self::$currentRuleSet->sum('counter'));
+//        $realPercentage = (self::$currentRuleSet->sum('counter') == 0) ? 0 : ($this->counter / self::$currentRuleSet->sum('counter'));
+        $realPercentage = $this->counter * self::$currentRuleSet->avg('counter');
         $ret = $plannedPercentage  - $realPercentage;
-        print("Priority for rule: {$this->name} is: $ret [$plannedPercentage/$realPercentage]");
+        print("Priority for rule: {$this->name} is: $ret [$plannedPercentage/$realPercentage]\n");
         return $ret;
     }
 
