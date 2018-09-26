d6ab21b17944a9644ce4647c3ddd9474f1f6cd73
Pavel Sokolov <comradepashka@gmail.com>
2018-09-26 18:51:30 +0300

    Mailkit: mail template wrap render() into print()

--- app/Console/Commands/MailkitHandleCommand.php
+++ app/Console/Commands/MailkitHandleCommand.php
@@ -75,7 +75,7 @@ class MailkitHandleCommand extends Command
                     }
                     print("mail from: {$mail->fromAddress} added using rule: {$rule->name} with priority: {$rule->priority}\n");
 //                    Mail::to($rule->recipient_list)->send(new ForwardedMail($source->login, $mail));
-                    (new ForwardedMail($source->login, $mail))->render();
+                    print((new ForwardedMail($source->login, $mail))->render());
                     print("\n---\n\n");
                     $log = new Log();
                     $log->from = $mail->fromAddress;
