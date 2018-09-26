abd613cb8adf86cdbaad32f629228f46f43e24e5
Pavel Sokolov <comradepashka@gmail.com>
2018-09-26 20:11:23 +0300

    Mailkit: mail template global regexp fix

--- app/Mail/Mailkit/ForwardedMail.php
+++ app/Mail/Mailkit/ForwardedMail.php
@@ -33,11 +33,17 @@ class ForwardedMail extends Mailable
     public function build()
     {
         $head = '';
-        $body = preg_replace('#(\n)#su', '<br />', $this->mail->textPlain);
+        $body = preg_replace('/(\n)/su', '<br />', $this->mail->textPlain);
         $bodyType = "plain";
-        if ($this->mail->textHtml) {
-            $head = preg_replace('#<head[^>]*>(.*?)<\/head>#isu', '$1', $this->mail->textHtml);
-            $body = preg_replace('#<body[^>]*>(.*?)<\/body>#isu', '$1', $this->mail->textHtml);
+        if ($html = $this->mail->textHtml) {
+            if (stripos($html, '<head')) {
+                $head = preg_replace('/<head[^>]*>(.*?)<\/head>/isu', '$1', $html);
+            }
+            if (stripos($html, '<body')) {
+                $body = preg_replace('/<body[^>]*>(.*?)<\/body>/isu', '$1', $html);
+            } else {
+                $body = preg_replace('/<html[^>]*>(.*?)<\/html>/isu', '$1', $html);
+            }
             $bodyType = "html";
         }
         print("BODY TYPE IS: $bodyType\n---\nHEAD:\n$head\n---\nBODY:\n$body\n---\n");
