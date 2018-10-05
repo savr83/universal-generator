8aa845ccbbb98b575ef3702e812b22c3e597f40e
Pavel Sokolov <comradepashka@gmail.com>
2018-10-06 01:02:28 +0300

    Mailkit Beta: blade templates fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                     DataBase Editor приложение Laravel+React!
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                         DataBase Editor приложение Laravel+React!
 
-                    <div id="example"></div>
-                    <script src="{{mix('js/app.js')}}" ></script>
+                        <div id="example"></div>
+                        <script src="{{asset(mix('js/app.js'))}}" ></script>
 
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
