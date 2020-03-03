ea9e3dd1484386432fa40b0258683af4a805ce35
ComradePashka <comradepashka@gmail.com>
2020-03-03 11:27:13 +0700

    Last stable version from old beget acc. Laravel = 5.6

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -41,6 +41,9 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
+//$app->withFacades();
+
+
 /*
 |--------------------------------------------------------------------------
 | Return The Application
