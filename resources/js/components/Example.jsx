29920d03625a3e0d732fa21a8f90ea02eb201f35
Pavel Sokolov <comradepashka@gmail.com>
2022-10-04 03:53:27 +0700

    mass movement

--- resources/js/components/Example.jsx
+++ resources/js/components/Example.jsx
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-function Example() {
-    return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">Example Component</div>
-
-                        <div className="card-body">I'm an example component!</div>
+class Example extends Component {
+    componentDidMount() {
+    }
+    render() {
+        return (
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                        <div className="card">
+                            <div className="card-header">Example Component</div>
+                            <div className="card-body">I'm an example component!</div>
+                            <button type="button" data-toggle="popover" data-placement="left" data-content="test lalala">Test</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Example;
 
-if (document.getElementById('example')) {
-    ReactDOM.render(<Example />, document.getElementById('example'));
+if (document.getElementById('root')) {
+    ReactDOM.render(<Example />, document.getElementById('root'));
 }
