39f86b05cbba0851a1055b655986445afe1a41e7
Pavel Sokolov <comradepashka@gmail.com>
2022-10-04 19:51:57 +0700

    vite fixes #a

--- docker/run.sh
+++ docker/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-#cd /app/public
-# php artisan migrate:fresh --seed
+#php artisan migrate:fresh --seed
 #php artisan migrate
+php artisan view:clear
 php artisan cache:clear
 php artisan route:cache
 
